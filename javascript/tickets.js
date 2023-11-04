96f456bc51302fdaef076708b7f2dd944c0034af
Pablo.A.M <pablo.mujica.trial@gmail.com>
2023-11-04 16:45:54 -0300

    Validados campos Nombre Apellido y Contraseña, sobre vacíos

--- javascript/tickets.js
+++ javascript/tickets.js
@@ -1,7 +1,12 @@
-document.getElementById("resumenx").addEventListener("click", calcular)
+// función del boton RESUMEN
+document.getElementById("resumenBtn").addEventListener("click", calcular)
+// función del botón BORRAR
 document.getElementById("borrar").addEventListener("click", borrar)
 
 let cantidad = 0;
+let nombre = "";
+let apellido = "";
+let correo = "";
 
 cantidad = document.getElementById("cantidad").value 
 console.log(cantidad)
@@ -31,14 +36,29 @@ select.addEventListener('change',
 function calcular(){
     
     cantidad = document.getElementById("cantidad").value;    
-    
-    if(cantidad < 1 || cantidad === "") {
+    nombre = document.getElementById("nombre").value
+    apellido = document.getElementById("apellido").value
+    correo = document.getElementById("correo").value
+
+    console.log("este es el nombre " + nombre)
+
+    if(nombre === "") {
+      document.getElementById('resu').innerHTML =  "Debe colocar SU NOMBRE"
+
+    } else if(apellido ==="") {
+      document.getElementById('resu').innerHTML =  "Debe colocar SU APELLIDO"
+
+    } else if(correo === "") {
+      document.getElementById('resu').innerHTML =  "Debe colocar SU CORREO"
+
+    } else if(cantidad < 1 || cantidad === "") {
         document.getElementById('resu').innerHTML = "Debe introducir una cantidad";
     } else {    
     resultado = cantidad * (200 - (200*descuento))
     document.getElementById('resu').innerHTML = resultado;   
    
-    console.log("Abona " + resultado) 
+    console.log("Abona " + resultado + "nombre" + nombre) 
+    console.log(nombre + apellido + correo)
     document.getElementById('resu').innerHTML = resultado + " (Importe total: $" + (200*cantidad) + ", descuento total = $" + (200*descuento*cantidad) + " ).";  
 }
 };
